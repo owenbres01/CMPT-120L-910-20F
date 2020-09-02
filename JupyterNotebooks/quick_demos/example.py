6ca40b9645b231496d8dff50e09eaa42981889a2
Aaron Michael Kippins <AKippins@hotmail.com>
2020-09-02 00:34:30 -0400

    Adding lab and class notes. Working on lesson two will post tomorrow.

---
+++ JupyterNotebooks/quick_demos/example.py
@@ -0,0 +1,8 @@
+
+def hello_thing(name, age):
+    print("Hello " + name + " I am " + str(age) + " years old.")
+
+
+print("Hello Class")
+hello_thing("Geoffery", 17)
+print("Hi", "my name is", "Paul")
