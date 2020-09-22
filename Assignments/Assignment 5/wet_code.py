2bbb71876b9cefc37735da0b113be80744bd101e
Aaron Michael Kippins <AKippins@hotmail.com>
2020-09-22 18:34:46 -0400

    Syncing up

---
+++ Assignments/Assignment 5/wet_code.py
@@ -0,0 +1,17 @@
+# We're looking to find out how much money we have after a day with friends on Saturday. 
+# Our code does the trick but we learned about keeping out code DRY recently and want to make it more efficent by making it DRY.
+# I want you to accomplish this by making functions where you see repeated code. 
+# Some things to note. When we have a positive number that gets split up and 85% goes into checking and 15% goes into savings. 
+# All negative numbers gets taken out of the checking account.
+
+
+def saturdays_bank_transactions(transations) -> (float, float):
+    savings = 1096.25
+    checking = 1590.80
+
+
+
+    pass
+if __name__ == "__main__":
+    transations = []
+    new_balance = saturdays_bank_transactions(transations)
