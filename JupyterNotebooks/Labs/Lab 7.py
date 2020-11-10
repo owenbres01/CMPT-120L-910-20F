7f2130b9cf6a7efc49cc04acd474c39609405f0e
owenbres01 <44271626+owenbres01@users.noreply.github.com>
2020-11-09 22:40:59 -0500

    Create eemoji
    emoji

---
+++ JupyterNotebooks/Labs/Lab 7.py
@@ -0,0 +1,9 @@
+import emoji
+
+print(emoji.emojize("I like python :red_heart:"))
+print(emoji.emojize('Water! :water_wave:'))
+print(emoji.emojize('This is going well :thumbs_up:'))
+
+print(emoji.demojize('This going well 👍'))
+print(emoji.demojize('Water! 🌊'))
+print(emoji.demojize("I like python ❤"))
