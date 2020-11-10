6928aa02766e286ab8b292563518c23ebd94a963
owenbres01 <44271626+owenbres01@users.noreply.github.com>
2020-11-09 22:28:55 -0500

    Lab

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 def index():
-    return '<h1>Hello!</h1>'
+    return '<h1>hello</h1>'
+
