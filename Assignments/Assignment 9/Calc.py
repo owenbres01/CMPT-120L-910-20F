e5b9947c202c1c3565bdd04820260766a63f77a9
owenbres01 <44271626+owenbres01@users.noreply.github.com>
2020-11-17 22:30:49 -0500

    calc

---
+++ Assignments/Assignment 9/Calc.py
@@ -0,0 +1,11 @@
+from Calculator import Calculator
+
+calc = Calculator()
+
+calc.addition(2, 7)
+calc.subtraction(5, 3)
+calc.multiplication(4, 3)
+calc.division(10, 5)
+calc.exponent(2, 4)
+calc.square_root(100)
+calc.negate(5)
