d8ce98c55fe0fcb1ac4cfceac2b0a402e2d4d9c5
Aaron Michael Kippins <AKippins@hotmail.com>
2020-11-20 14:10:03 -0500

    Syncing Lesson 10

---
+++ Assignments/Assignment 9/calc_solution.py
@@ -0,0 +1,11 @@
+from Calculator import Calculator
+
+calc = Calculator()
+
+print(calc.addition(1, 2))
+print(calc.subtraction(2, 1))
+print(calc.multiplication(2, 2))
+print(calc.division(4, 2))
+print(calc.exponent(2, 3))
+print(calc.square_root(4))
+print(calc.negate(10))
