a7377baa6ea3d4811ed742ec2564f425ab4ed774
owenbres01 <44271626+owenbres01@users.noreply.github.com>
2020-12-09 19:49:40 -0500

    flask

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -8,6 +8,18 @@ def hello():
     app.logger.error('Processing default request')
     return render_template('index.html')
 
+@app.route('/AboutMe')
+def about_me():
+    return render_template('index2.html')
+
+@app.route('/Contact')
+def contact():
+    return render_template('index3.html')
+
+@app.route('/MyPersonality')
+def my_personality():
+    return render_template('index4.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
